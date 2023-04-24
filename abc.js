8fee193dea0ee7f3e085a5f008a95994d3856360
Syed Ali Hamza Naqvi <129213252+alihamza48@users.noreply.github.com>
2023-04-25 00:55:41 +0500

    Add files via upload

---
+++ abc.js
@@ -0,0 +1,29 @@
+function changeColor(){
+   let b =  document.querySelector('.h')
+   
+   if(b.style.color === "black")
+   {
+       b.style.color = "red";
+   }
+   else if (b.style.color !== "black")
+   {
+    b.style.color = "black"
+   }
+}
+function use(){
+    let a= document.querySelector("ul")
+ 
+    if(a.style.display === "none")
+    {
+       a.style.display =  "flex"
+   }
+   else if(a.style.display !== "none")
+   {
+       a.style.display = "none"
+   }
+   
+}
+
+
+
+
